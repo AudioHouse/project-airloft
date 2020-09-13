958924bd8892e49f3931c440090f97d8ced95a16
Austin Miller <austinmiller360@gmail.com>
2020-09-12 20:27:55 -0700

    Group resources now require a valid jwt

--- server/api/routes/group.route.js
+++ server/api/routes/group.route.js
@@ -1,19 +1,25 @@
 const GroupsController = require('../controllers/group.controller');
+const AuthValidation = require('../../common/middlewares/auth.validation.middleware');
 
 exports.routesConfig = (app) => {
     app.post('/api/groups', [
+        AuthValidation.hasJwtPresent,
         GroupsController.insert
     ]);
     app.get('/api/groups/:groupId', [
+        AuthValidation.hasJwtPresent,
         GroupsController.getById
     ]);
     app.patch('/api/groups/:groupId', [
+        AuthValidation.hasJwtPresent,
         GroupsController.patchById
     ]);
     app.get('/api/groups', [
+        AuthValidation.hasJwtPresent,
         GroupsController.getAllGroups
     ]);
     app.delete('/api/groups/:groupId', [
+        AuthValidation.hasJwtPresent,
         GroupsController.removeGroupById
     ]);
 }
